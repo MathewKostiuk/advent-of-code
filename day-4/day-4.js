38259ffb2cd802c1af582c3e636f1dcea50b5cbf
Mathew Kostiuk <mathewkostiuk@gmail.com>
2019-12-07 22:29:35 +0900

    completed day 4 part 2

--- day-4/day-4.js
+++ day-4/day-4.js
@@ -3,15 +3,27 @@ const input = [206938, 679128];
 const isValid = (password) => {
   let twoAdjacent = false;
   let onlyIncreasing = true;
+  let adjacents = {};
 
   for (let i = 0; i < password.length; i++) {
     if (password[i] === password[i + 1]) {
       twoAdjacent = true;
+      if (adjacents.hasOwnProperty(password[i])) {
+        adjacents[password[i]]++;
+      } else {
+        adjacents[password[i]] = 1;
+      }
     }
 
     if (Number(password[i]) > Number(password[i + 1])) {
       onlyIncreasing = false;
     }
+  }
+  if (Object.values(adjacents).indexOf(2) !== -1 && Object.values(adjacents).indexOf(1) === -1) {
+    twoAdjacent = false;
+  }
+  if (Math.max(Object.values(adjacents)) > 1 && Object.values(adjacents).indexOf(1) === -1) {
+    twoAdjacent = false;
   }
   return twoAdjacent && onlyIncreasing;
 };
@@ -25,6 +37,10 @@ const createPossiblePasswords = (input) => {
     }
   }
   return possibleCombinations;
+}
+
+const countAdjacents = (password) => {
+
 }
 
 console.log(createPossiblePasswords(input));
