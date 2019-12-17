8ee178b201f2ff5266b81d33a87d463f580bdea3
Mathew Kostiuk <mathewkostiuk@gmail.com>
2019-12-17 19:32:26 +0900

    completed day 8 part 2

--- day-8/day-8.js
+++ day-8/day-8.js
@@ -13,36 +13,59 @@ const createImage = (inputs, width, height) => {
   const layerSize = width * height;
   const image = [];
   const result = [];
+  const indicesOfTwo = [];
+  let cloneOfFirstLayer;
 
   for (let i = 0; i < inputs.length; i += layerSize) {
     const layer = [];
     let indexCounter = i;
-    let layerCounter = i + width;
-
+    let layerCounter = i + layerSize;
     let zeroes = 0;
     let ones = 0;
     let twos = 0;
-    while (layer.length < 6) {
-      const substring = inputs.substring(indexCounter, layerCounter);
-      layer.push(substring);
-      zeroes += countDigits(0, substring);
-      ones += countDigits(1, substring);
-      twos += countDigits(2, substring);
-      layerCounter += width;
-      indexCounter += width;
+
+    const substring = inputs.substring(indexCounter, layerCounter);
+    if (i === 0) {
+      cloneOfFirstLayer = substring.split('');
     }
+    layer.push(substring);
+
+    for (let j = 0; j < substring.length; j++) {
+      if (substring[j] === '2' && i === 0) {
+        indicesOfTwo.push(j);
+      }
+      if (i !== 0) {
+        if (substring[j] !== '2' && cloneOfFirstLayer[j] === '2') {
+          cloneOfFirstLayer[j] = substring[j];
+        }
+      }
+
+    }
+
+    zeroes += countDigits(0, substring);
+    ones += countDigits(1, substring);
+    twos += countDigits(2, substring);
+
     image.push(layer);
     result.push([zeroes, ones * twos]);
   }
-  return result.sort((a, b) => {
+  const newImage = [];
+  for (let k = 0; k < cloneOfFirstLayer.length; k += width) {
+    newImage.push(cloneOfFirstLayer.slice(k, k + width));
+  }
+
+  const partOneSolution = result.sort((a, b) => {
     return a[0] - b[0];
   });
+  const imageResult = newImage.map((row) => row.map((c) => c === '0' ? ' ' : 'X').join('')).join('\n');
+
+  return [partOneSolution, imageResult];
 }
 
-const partOne = () => {
+const solutions = () => {
   const inputs = readInputs();
-  const result = createImage(inputs, 25, 6);
-  return result[0][1];
+  const [partOneSolution, imageResult] = createImage(inputs, 25, 6);
+  return `this is part one: ${partOneSolution[0][1]}  and this is part 2: \n ${imageResult}`;
 }
 
-console.log(partOne());
+console.log(solutions());
